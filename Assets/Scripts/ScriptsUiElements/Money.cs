b62c5ea848b35dbcd929fb081922dee6d488eb9f
Juan Pablo <jcamacho249@unab.edu.co>
2020-04-05 14:36:20 -0500

    Prefabs de UI elements & bugs
    *Prefabs de la barra. de energía y el contador de dinero.
    *Prefabs de los bichos
    *Se actualizan prefabs de desktop items. Se les agrega un collider 3D ( para que interactuén con los bichos)
    *Animators para los bugs y el coffee

---
+++ Assets/Scripts/ScriptsUiElements/Money.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Money : MonoBehaviour
+{
+    public TextMesh textmesh;
+
+    public int money=350;
+
+    void Start()
+    {
+        textmesh = GetComponent<TextMesh>();
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        money++;
+
+        textmesh.text = ("$"+money.ToString());
+        
+       
+         }
+
+        void Predetermined()
+        {
+        textmesh.text = ("350");       
+        }
+
+
+}
+
+
