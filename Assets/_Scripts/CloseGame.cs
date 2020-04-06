4b443398c8333d2a1dc090d210c2740bdbfaf4da
Q-ro <andres.mrad@gmail.com>
2020-04-06 17:21:12 -0500

    Added Main Menu

---
+++ Assets/_Scripts/CloseGame.cs
@@ -0,0 +1,15 @@
+﻿/*
+Author: Andres Mrad (Q-ro)
+Date: Monday 06/April/2020 @ 17:01:44
+Description:  Shutsdown the game and exist the unity application 
+*/
+
+using UnityEngine;
+
+public class CloseGame : MonoBehaviour
+{
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
