b1fd87811e44f2dd2a273348205c6a34aa50c02a
Q-ro <andres.mrad@gmail.com>
2020-04-06 15:15:54 -0500

    Modified ingametimer classne
    Sawpped the way the ingame timer was working because, fuck it

--- Assets/_Scripts/ScriptsUiElements/InGameTimer.cs
+++ Assets/_Scripts/ScriptsUiElements/InGameTimer.cs
@@ -4,6 +4,7 @@ Date: Monday 06/April/2020 @ 02:22:14
 Description:  Keeps track of the current ingame time
 */
 
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -16,28 +17,31 @@ public class InGameTimer : MonoBehaviour
 
     [SerializeField] private Text clockDisplay;
     [SerializeField] private int timePassIncrements = 10;
-    [SerializeField] private int startingDay = 1;
+    // [SerializeField] private int startingDay = 1;
 
 
-    [Range(0, 23)]
-    [SerializeField] private int startingHour = 6;
+    // [Range(0, 23)]
+    // [SerializeField] private int startingHour = 6;
 
-    [Range(0, 59)]
-    [SerializeField] private int startingMinute = 0;
+    // [Range(0, 59)]
+    // [SerializeField] private int startingMinute = 0;
+
+    [SerializeField] private DateTime startingGameDate = new DateTime(2020, 04, 01, 06, 00, 00);
 
     #endregion
 
     #region Private Variables
+    private DateTime _currentIngameDateTime; // The current ingame date & time
 
-    private int _currentDay;
-    private int _currentHour;
-    private int _currentMinute;
+    // private int _currentDay;
+    // private int _currentHour;
+    // private int _currentMinute;
 
     private bool _isClockTicking = false;
 
-    public int CurrentDay { get => _currentDay; }
-    public int CurrentHour { get => _currentHour; }
-    public int CurrentMinute { get => _currentMinute; }
+    public int CurrentDay { get => _currentIngameDateTime.Day; }
+    public int CurrentHour { get => _currentIngameDateTime.Hour; }
+    public int CurrentMinute { get => _currentIngameDateTime.Minute; }
 
     #endregion
 
@@ -82,34 +86,38 @@ public class InGameTimer : MonoBehaviour
 
     private void InitVariables()
     {
-        this._currentDay = this.startingDay;
-        this._currentHour = this.startingHour;
-        this._currentMinute = this.startingMinute;
+        // this._currentDay = this.startingDay;
+        // this._currentHour = this.startingHour;
+        // this._currentMinute = this.startingMinute;
+
+        this._currentIngameDateTime = this.startingGameDate;
         this._isClockTicking = false;
         UpdateClockDisplay();
     }
 
     private void UpdateClockDisplay()
     {
-        clockDisplay.text = string.Format("{2:D2} | {0:D2} : {1:D2}", this._currentHour, this._currentMinute, this._currentDay);
+        clockDisplay.text = string.Format("{2:D2} | {0:D2} : {1:D2}", this._currentIngameDateTime.Hour, this._currentIngameDateTime.Minute, this._currentIngameDateTime.Day);
     }
 
     private void TickTimer()
     {
-        var previousMinute = this._currentMinute;
-        // Increase minutes
-        this._currentMinute = HelpMethods.WrapNumber(0, 59, this._currentMinute + this.timePassIncrements);
-        if (previousMinute > this._currentMinute)
-        {
-            // Increase Hours
-            IncreaseTimerHours();
-        }
+        // var previousMinute = this._currentMinute;
+        // // Increase minutes
+        // this._currentMinute = HelpMethods.WrapNumber(0, 59, this._currentMinute + this.timePassIncrements);
+        // if (previousMinute > this._currentMinute)
+        // {
+        //     // Increase Hours
+        //     IncreaseTimerHours();
+        // }
+
+        this._currentIngameDateTime = this._currentIngameDateTime.AddMinutes(10);
     }
 
     private void IncreaseTimerHours(int times = 1)
     {
         // Increase Hours
-        this._currentHour = HelpMethods.WrapNumber(0, 24, this._currentHour + 1 * times);
+        // this._currentHour = HelpMethods.WrapNumber(0, 24, this._currentHour + 1 * times);
         // if (this._currentHour > HelpMethods.WrapNumber(0, 59, this._currentHour + 1 * times))
         // {
         //     this._currentHour = HelpMethods.WrapNumber(0, 59, this._currentHour + 1 * times);
