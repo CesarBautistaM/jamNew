abe4a134e36e7705273d8bc0f731f038bc332d7c
Q-ro <andres.mrad@gmail.com>
2020-04-06 19:24:41 -0500

    Credits, the DPKR edition
    added credits n' shit

---
+++ Assets/_Scripts/CreditImageController.cs
@@ -0,0 +1,38 @@
+﻿/*
+Author: Andres Mrad (Q-ro)
+Date: Monday 06/April/2020 @ 18:37:21
+Description:  Handles the interaction between  
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class CreditImageController : MonoBehaviour, IPointerClickHandler
+{
+
+    #region Inspector Variables
+
+    [SerializeField] private GameObject hiddenContent = null;
+    [SerializeField] private Text creditsInfoDisplay;
+    [SerializeField] private string nombreCredits;
+    [SerializeField] private string rolCredits;
+
+    #endregion
+
+    #region Private Variables
+
+    #endregion
+
+
+    void IPointerClickHandler.OnPointerClick(PointerEventData eventData)
+    {
+        this.creditsInfoDisplay.text = string.Format("Nombre: {0}\nRol: {1}", this.nombreCredits, this.rolCredits);
+
+        if (hiddenContent)
+            hiddenContent.SetActive(true);
+    }
+
+}
